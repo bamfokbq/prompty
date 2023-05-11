a36f4b8bfcc8a8cdc027ceab1b9de60e61e5696b
Kofi Bamfo Quaicoe <33463657+bamfokbq@users.noreply.github.com>
2023-05-11 12:23:25 +0000

    added landing page

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import Nav from '@components/Nav';
+import Provider from '@components/Provider';
+import '@styles/globals.css';
+
+export const metadata = {
+  title: 'Prompty',
+  description: 'Discover & share AI prompts',
+};
 
 const RootLayout = ({ children }) => {
   return (
     <html lang='en'>
-      <main>{children}</main>
+      <Provider>
+        <body>
+          {/* <div className='main'>
+          <div className='gradient' />
+        </div> */}
+
+          <main>
+            <Nav />
+            {children}
+          </main>
+        </body>
+      </Provider>
     </html>
   );
 };
